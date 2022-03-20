76e36687562759124d18b89786ddc29383a301d1
Marc Weitz <marc.weitz@onlinehome.de>
2022-03-20 12:23:03 +0100

    cleans up test

--- tests/test_features.py
+++ tests/test_features.py
@@ -13,15 +13,17 @@ from paat import io, features
 def test_calculate_actigraph_counts(load_gt3x_file, test_root_path):
     data, sample_freq = load_gt3x_file
 
+    # Test 1sec count processing
     counts_1s = features.calculate_actigraph_counts(data, sample_freq, 1)
-    counts_10s = features.calculate_actigraph_counts(data, sample_freq, 10)
-
     ref_1s = pd.read_csv(os.path.join(test_root_path, 'resources/10min_recording1sec.csv'),
                          skiprows=10, header=None, names=["Y", "X", "Z", "Steps"])
-    ref_10s = pd.read_csv(os.path.join(test_root_path, 'resources/10min_recording10sec.csv'),
-                          skiprows=10, header=None, names=["Y", "X", "Z", "Steps"])
 
     npt.assert_almost_equal(counts_1s[["X", "Y", "Z"]].values, ref_1s[["X", "Y", "Z"]].values)
+
+    # Test 10sec count processing
+    counts_10s = features.calculate_actigraph_counts(data, sample_freq, 10)
+    ref_10s = pd.read_csv(os.path.join(test_root_path, 'resources/10min_recording10sec.csv'),
+                          skiprows=10, header=None, names=["Y", "X", "Z", "Steps"])
 
     npt.assert_almost_equal(counts_10s[["X", "Y", "Z"]].values, ref_10s[["X", "Y", "Z"]].values)
 
