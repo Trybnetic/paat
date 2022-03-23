069ca8aa637a92b5eb8b66f779f2e9aee5a664ad
Marc Weitz <marc.weitz@onlinehome.de>
2022-03-23 15:22:35 +0100

    renames contents to index and declares master doc in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,9 @@ author = 'Marc Weitz & Shaheen Syed'
 
 # -- General configuration ---------------------------------------------------
 
+# Set master doc name
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
